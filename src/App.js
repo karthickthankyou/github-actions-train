92c90dbb1c69d52e39ad94d8b706570fce0fd480
Karthick Ragavendran <karthickthankyou@gmail.com>
2021-05-18 12:59:50 +0530

    fix: skipped lint-staged in husky/pre-commit due to tsc incompatibility

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from "react";
-
+"use strict";
+exports.__esModule = true;
+var react_1 = require("react");
 function App() {
   return (
     <div className="p-10 m-10 text-5xl text-red-500 bg-yellow-100">
@@ -7,5 +8,4 @@ function App() {
     </div>
   );
 }
-
-export default App;
+exports["default"] = App;
