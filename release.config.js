3eecdf4b4c44806152b61acc21f64d505281b1c2
Karthick Ragavendran <karthickthankyou@gmail.com>
2021-05-18 14:16:29 +0530

    fix: updated release config

--- release.config.js
+++ release.config.js
@@ -4,6 +4,6 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/git',
+    '@semantic-release/github',
   ],
 }
