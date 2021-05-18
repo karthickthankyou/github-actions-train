b35c1c083d0667beab23adca97e98e169fa8e868
Karthick Ragavendran <karthickthankyou@gmail.com>
2021-05-18 13:46:01 +0530

    fix: implemented semantic release

---
+++ release.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  branches: 'main',
+  repositoryUrl: 'https://github.com/karthickthankyou/youtube-clone-train',
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/git',
+  ],
+}
