6d358a72bd9535efef06edfdcf1e0a8318f807b8
Karthick Ragavendran <karthickthankyou@gmail.com>
2021-05-21 10:10:30 +0530

    fix: fixed the test ui component

--- src/TestUI/TestUI.tsx
+++ src/TestUI/TestUI.tsx
@@ -5,6 +5,14 @@ const TestUI = ({}: ITestUIProps) => {
     <div>
       Hello, This is TestUI component. Updated with this text.
       <span>This is so cool!</span>
+      <div
+        style={{
+          height: '400px',
+          width: '200px',
+          padding: '20px',
+          background: '#ff0000',
+        }}
+      ></div>
     </div>
   )
 }
