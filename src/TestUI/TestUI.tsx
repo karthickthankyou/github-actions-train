5ef09d435bc4cc5176f9b7ad1da1625e7c043129
Karthick Ragavendran <karthickthankyou@gmail.com>
2021-05-21 10:02:57 +0530

    fix: added more text for chromatic review

--- src/TestUI/TestUI.tsx
+++ src/TestUI/TestUI.tsx
@@ -1,7 +1,12 @@
 export interface ITestUIProps {}
 
 const TestUI = ({}: ITestUIProps) => {
-  return <div>Hello, This is TestUI component. Updated with this text.</div>
+  return (
+    <div>
+      Hello, This is TestUI component. Updated with this text.
+      <span>This is so cool!</span>
+    </div>
+  )
 }
 
 export default TestUI
