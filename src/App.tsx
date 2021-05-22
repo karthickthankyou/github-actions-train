353e0da4f46748deef9ec8a5e90bca65c327b136
Karthick Ragavendran <karthickthankyou@gmail.com>
2021-05-22 08:03:50 +0530

    ci: testing amplify deploy

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="p-10 m-10 text-5xl text-red-500 bg-yellow-100">
       Hello World. This is great! Testing lint staged again!
+      <span className="bg-yellow-300">This is amazing</span>
     </div>
   )
 }
